313bf076582d55c4bb111f39ad3f8318e82db1c0
eelysee <135763902+eelysee@users.noreply.github.com>
2023-09-06 16:59:43 -0500

    models

--- adam_wrangle.py
+++ adam_wrangle.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from env import get_connection
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 def get_grades():
@@ -51,3 +52,28 @@ def train_val_test(df, seed = 42):
                                  random_state = seed)
     
     return train, val, test
+
+
+def xy_split(df):
+    
+    return df.drop(columns=['price']), df.price
+
+
+def scale_data(train, val, test, to_scale):
+    #make copies for scaling
+    train_scaled = train.copy()
+    validate_scaled = val.copy()
+    test_scaled = test.copy()
+
+    #make the thing
+    scaler = MinMaxScaler()
+
+    #fit the thing
+    scaler.fit(train[to_scale])
+
+    #use the thing
+    train_scaled[to_scale] = scaler.transform(train[to_scale])
+    validate_scaled[to_scale] = scaler.transform(val[to_scale])
+    test_scaled[to_scale] = scaler.transform(test[to_scale])
+    
+    return train_scaled, validate_scaled, test_scaled
